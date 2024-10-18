5e49266cd7720cc5f0946a622aba8d8d01df41cc
shreya.yadava@gmail.com <shreya.yadava@gmail.com>
2024-10-18 08:55:21 +0530

    jss_1

---
+++ jss_work/index.js
@@ -0,0 +1,13 @@
+// console.group("hi");
+// const a=12;
+// if(a>10){
+//    a=23;
+//     console.log("a=",a);
+// }
+// console.log('hi '+ a);
+function fun(a,b){
+    console.log("hi js ");
+    return a+b;
+}
+let data =fun(3,1);
+console.log(data);
